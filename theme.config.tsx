c913a28125a247737a29b4dde08ecf114dae98bd
iw19 <126999343+iw19@users.noreply.github.com>
2023-03-05 11:47:26 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { DocsThemeConfig } from "nextra-theme-docs"
 
 const config: DocsThemeConfig = {
-    logo: <span>Vscode 群文档</span>,
+    logo: <span>妮可 VS Code 群文档</span>,
     project: {
         link: "https://github.com/iw17/vscode",
     },
